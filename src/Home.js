b00ad579357141bd5418ca1a84e272010ec27631
RijlanM10 <rijlan01@gmail.com>
2021-03-04 21:55:49 +0700

    Reusing Components

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" />
+            <BlogList blogs={blogs.filter(blog => blog.author === 'clinchy')} title="Clinchy's Blogs" />
         </div>
     );
 }
