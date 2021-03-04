d476accbb67477161b2de786e32cc543b049f9e2
RijlanM10 <rijlan01@gmail.com>
2021-03-04 22:04:20 +0700

    Functions as Props

--- src/Home.js
+++ src/Home.js
@@ -7,11 +7,15 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum ...', author: 'rii', id: 2 },
         { title: 'Web dev pro tips', body: 'lorem ipsum ...', author: 'clinchy', id: 3 },
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
     
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" />
-            <BlogList blogs={blogs.filter(blog => blog.author === 'clinchy')} title="Clinchy's Blogs" />
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
         </div>
     );
 }
