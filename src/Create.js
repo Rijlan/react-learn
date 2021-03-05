39a31a75723e2e29b06ab0d6f3ac9ee2d1ae4ddb
RijlanM10 <rijlan01@gmail.com>
2021-03-05 11:44:33 +0700

    Programmatic Redirects

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('clinchy');
     const [isLoading, setIsLoading] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,6 +22,8 @@ const Create = () => {
         }).then(() => {
             console.log('new blog added');
             setIsLoading(false);
+            // history.go(-1);
+            history.push('/');
         })
     }
 
