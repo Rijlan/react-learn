ff1caf8181154421db469905eec0dabecd85f4e2
RijlanM10 <rijlan01@gmail.com>
2021-03-05 09:00:08 +0700

    Fetching Data with useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
 // const BlogList = (props) => {
     // const blogs = props.blogs;
     // const title = props.title;
@@ -10,7 +10,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
